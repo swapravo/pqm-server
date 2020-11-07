6fe4665d47dd5a2936318dcf84b5ee5580795b82
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-11-07 22:20:08 +0530

    Minor fixes...

--- src/requests.py
+++ src/requests.py
@@ -1,6 +1,7 @@
 import src.globals
 import src.network
 import src.crypto
+import src.utils
 
 
 def username_availability_check(connection, request):
@@ -18,33 +19,33 @@ def username_availability_check(connection, request):
 
 	try:
 		if not (
-			instanceof(request["username"], str) and
+			isinstance(request["username"], str) and
 			src.utils.username_validity_checker(request["username"]) and
-			instanceof(request["rolling_public_key"], bytes) and
+			isinstance(request["rolling_public_key"], bytes) and
 			src.crypto.validate_key(request["rolling_public_key"])):
-
 			src.network.block(connection)
 			return
 			# kill process here
 	except:
-		src.network.block(connection)
+		src.network.block(connection, src.globals.STRANGER_TTL, True)
 		return
 		# kill process here
 
 	response = src.db.is_username_available(request["username"])
 
 	# pack and send this response to the client
-	response = {"nonce": request["nonce"], "response": response}
+	response = { \
+		"nonce": request["nonce"], \
+		"response": response}
 
 	response = src.crypto.asymmetrically_encrypt(src.utils.pack(response), request["rolling_public_key"])
 
 	response = src.utils.pack({"token": src.crypto.hash(response), "response": response})
-	response = (len(response)).to_bytes(4, byteorder='little') + response
+	response = src.utils.sizeof(response) + response
 
 	# insert timeout here!
 	connection.sendall(response)
-
-	print("USERNAME_AVAILABILITY_CHECK PROCESSED SUCCESSFULLY!")
+	print("Processed a signup request!")
 
 
 def signup(connection, request):
@@ -61,11 +62,11 @@ def signup(connection, request):
 
 	try:
 		if not (
-			instanceof(request["username"], str) and
+			isinstance(request["username"], str) and
 			src.utils.username_validity_checker(request["username"]) and
-			instanceof(request["encryption_public_key"], bytes) and
+			isinstance(request["encryption_public_key"], bytes) and
 			src.crypto.validate_key(request["encryption_public_key"]) and
-			instanceof(request["signature_public_key"], bytes) and
+			isinstance(request["signature_public_key"], bytes) and
 			src.crypto.validate_key(request["signature_public_key"])):
 
 			src.network.block(connection)
@@ -104,11 +105,8 @@ def login(connection, request):
 	STRUCTURE OF A REQUEST
 	request = {
 		"username": utf-8 string
-		"token": bytes, SIGNED WHAT???
-
-	}
+		"token": bytes, SIGNED WHAT???}
 	"""
-
 	pass
 
 
@@ -118,131 +116,140 @@ def reconnect(connection, request):
 
 def process(connection):
 
+	ip, port = connection.getpeername()
+	port = str(port)
+
 	# THROTTLE REQUESTS
 	# PREFERABLY FROM THE src.network.recieve fn
 
 	while True:
 		request = src.network.recieve(connection, src.globals.DMZ_BUFFER_SIZE)
+
 		if request:
 			# feeding dmz data straight to this function
 			# is that safe?
 			request = src.utils.unpack(request)
-			if not instanceof(request, bytes):
+			if not isinstance(request, dict):
+				print("Unpacking FAILED")
 				src.network.close(connection)
 				return
 				# kill process here
 
-	"""
-	STRUCTURE OF A REQUEST
-	request = {
-		version: int, version of the client
-		token: dict = {
-						"type": str, a "hash" or a "sign"ed hash,
-						"token": bytes }
-				## NOTE: we have to allow unsigned hashes because
-				creating such a signature needs keys, generating which
-				are computationally hard
-		request: bytes, asymmetrically encrypted, actual request }
-	"""
-
-	# validating supplied data here
-	try:
-		if not (instanceof(request["vesion"], int) and
-			(request["token"]["type"] == "hash" or
-			request["token"]["type"] == "sign") and
-			instanceof(request["request"], bytes)):
-			src.network.block(connection)
-			return
-			# kill process here
-	except:
-		src.network.block(connection)
-		return
-		# kill process here
-
-	# try except key error return
-	# and handle version specific stuff!
-	if request["version"] not in src.globals.SUPPORTED_VERSIONS:
-		connection.sendall(src.globals.UNSUPPORTED_VERSION)
-		src.network.close(connection)
-		return
-		# kill process here
-
-	token = request["token"]
-	if token["type"] == "hash":
-		if src.crypto.hash(request) != token["token"]:
-			# hash verification FAILED
-			src.network.close(connection)
-			# kill process here
-			return
-
-	request = src.crypto.asymmetrically_decrypt(request["request"])
-
-	if isinstance(request, bytes):
-		request = src.utils.unpack(request)
-		if not instanceof(request, dict):
-			src.network.close(connection)
-			# kill process here
-			return
-	else:
-		src.network.close(connection)
-		# kill process here
-		return
-
-	"""
-	STRUCTURE OF THE INNER REQUEST
+		"""
+		STRUCTURE OF A REQUEST
 		request = {
-			timestamp: bytes, an UNIX timestamp
-			request_code: bytes, a src.globals code
-			nonce: bytes
-			request: dict, contains all necessary info to perfom the request
-				}
-	"""
+			version: str, version of the client
+			token: dict = {
+							"type": str, a "hash" or a "sign"ed hash,
+							"token": bytes }
+					## NOTE: we have to allow unsigned hashes because
+					creating such a signature needs keys, generating which
+					are computationally hard
+			request: bytes, asymmetrically encrypted, actual request }
+		"""
 
-	try:
-		if not (len(request["timestamp"]) == 4 and
-			# and request_code is a valid code
-			instanceof(request["request_code"], bytes) and
-			 len(request["request_code"]) == 2 and
-			instanceof(request["request_code"], bytes) and
-			len(request["request_code"]) == sc.globals.NONCE_SIZE and
-			instanceof(request["request"], dict)):
+		# validating supplied data here
+		try:
+			print(request)
+			if not (isinstance(request["version"], str) and
+				(request["token"]["type"] == "hash" or
+				request["token"]["type"] == "sign") and
+				isinstance(request["request"], bytes)):
+				src.network.block(connection, src.globals.HOUR, block_ip_plus_port=True)
+				return
+				# kill process here
+
+		except:
+			src.network.block(connection, src.globals.HOUR, block_ip_plus_port=True)
+			return
+			# kill process here
+
+		# try except key error return
+		# and handle version specific stuff!
+		if request["version"] not in src.globals.SUPPORTED_VERSIONS:
+			connection.sendall(src.globals.UNSUPPORTED_VERSION)
+			src.network.close(connection)
+			return
+			# kill process here
+
+		token = request["token"]
+		if token["type"] == "hash":
+			if src.crypto.hash(request["request"]) != token["token"]:
+				# hash verification FAILED
+				src.network.close(connection)
+				# kill process here
+				return
+
+		request = src.crypto.asymmetrically_decrypt(request["request"], src.globals.SERVER)
+
+		if isinstance(request, bytes):
+			request = src.utils.unpack(request)
+			if not isinstance(request, dict):
+				src.network.close(connection)
+				# kill process here
+				return
+		else:
+			src.network.close(connection)
+			# kill process here
+			return
+
+
+		"""
+		STRUCTURE OF THE INNER REQUEST
+			request = {
+				timestamp: bytes, an UNIX timestamp
+				nonce: bytes
+				request_code: bytes, a src.globals code
+				request: dict, contains all necessary info to perfom the request
+					}
+		"""
+
+		try:
+			if not (len(request["timestamp"]) == 4 and
+				# and request_code is a valid code
+				isinstance(request["request_code"], bytes) and
+				len(request["request_code"]) == 2 and
+				isinstance(request["nonce"], bytes) and
+				len(request["nonce"]) == src.globals.NONCE_SIZE and
+				isinstance(request["request"], dict)):
+				src.network.block(connection, src.globals.HOUR, block_ip_plus_port=True)
+
+				return
+				# kill process here
+		except:
+			src.network.block(connection, src.globals.HOUR, block_ip_plus_port=True)
+			return
+			# kill process here
+
+		if src.utils.timedelta(src.utils.timestamp(), request["timestamp"]) > \
+			src.globals.MAX_ALLOWABLE_TIME_DELTA or src.db.nonces.exists(request["nonce"]):
+
+			# should i block this user for a few minutes
+			# or just close the connection
+			# Or should i block this IP instead?
+			src.network.block(ip+':'+port, src.globals.MAX_ALLOWABLE_TIME_DELTA)
+			return
+
+		src.db.nonces.set(request["nonce"], 0, ex=src.globals.MAX_ALLOWABLE_TIME_DELTA)
+		request["request"]["nonce"] = request["nonce"]
+
+		if request["request_code"] == src.globals.USERNAME_AVAILABILITY_CHECK:
+			src.requests.username_availability_check(connection, request["request"])
+
+		elif request["request_code"] == src.globals.SIGNUP:
+			src.requests.signup(connection, request["request"])
+
+		elif request["request_code"] == src.globals.LOGIN_STEP_1:
+			request["token"] = token["token"]
+			src.requests.login(connection, request["request"])
+
+		elif request["request_code"] == src.globals.RECONNECT:
+			src.requests.reconnect(connection, request["request"])
+
+		else:
 			src.network.block(connection)
 			return
 			# kill process here
-	except:
-		src.network.block(connection)
-		return
-		# kill process here
-
-	if src.utils.timedelta(src.utils.timestamp(), request["timestamp"]) > \
-		src.globals.MAX_ALLOWABLE_TIME_DELTA or src.db.nonces.exists(request["nonce"]):
-
-		# should i block this user for a few minutes
-		# or just close the connection
-		# Or should i block this IP instead?
-		src.network.block(ip+':'+port, src.globals.MAX_ALLOWABLE_TIME_DELTA)
-		return
-
-	src.db.nonces.set(request["nonce"], 0, ex=src.globals.MAX_ALLOWABLE_TIME_DELTA)
-	request["request"]["nonce"] = request["nonce"]
-
-	if request["request_code"] == src.globals.USERNAME_AVAILABILITY_CHECK:
-		src.requests.username_availability_check(connection, request["request"])
-
-	elif request["request_code"] == src.globals.SIGNUP:
-		src.requests.signup(connection, request["request"])
-
-	elif request["request_code"] == src.globals.LOGIN_STEP_1:
-		request["token"] = token["token"]
-		src.requests.login(connection, request["request"])
-
-	elif request["request_code"] == src.globals.RECONNECT:
-		src.requests.reconnect(connection, request["request"])
-
-	else:
-		src.network.block(connection)
-		return
-		# kill process here
 
 
 """
