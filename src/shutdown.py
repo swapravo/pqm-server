5dc7e11b05a2d9c1c20f6a14855c008c786b8b07
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-11-18 13:54:48 +0530

    shutdown,process is executed after a request is done processing. Its more of a explicit garbage cleaner. shutdown.process is a template for a function that'll shut the process down.

--- src/shutdown.py
+++ src/shutdown.py
@@ -1,2 +1,11 @@
-def shutdown():
-    print("Shutting threads down...")
+from time import sleep
+
+
+def server():
+    print("Shutting server down...")
+    pass
+
+
+def process():
+    print("kill process here!")
+    sleep(60*60*24)
