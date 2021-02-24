c1581d07b85c338ac50046c62867a5a5c37c09ed
Swapravo Sinha Roy <swapravo@tutanota.com>
2021-02-24 13:32:11 +0530

    Shortened the server code

--- src/server.py
+++ src/server.py
@@ -1,17 +1,8 @@
-"""
-RIGHT NOW, WE ARE USING IP+PORT TO "IDENTIFY" CONNECTIONS.
-THIS CAN CAUSE ISSUES... FOR EXAMPLE, IF TWO USERS ON A
-SHARED IP START THE CLIENT APP ON THE SAME PORT, IT'LL
-PROBABLY CAUSE SOMETHING LIKE A CROSS CONNECTION.
-WE NEED TO FIGURE OUT SOMETHING FOR THIS.
-"""
-
-import multiprocessing
-import socket
+from multiprocessing import Process
+from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, SHUT_RDWR, socket
 
 import src.shutdown
-import src.requests
-import src.db
+import src.db.blacklist
 import src.client
 
 
@@ -21,50 +12,25 @@ class Server():
 		self.port = port
 
 	def start(self):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket = socket(AF_INET, SOCK_STREAM)
+		self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.socket.bind((self.hostname, self.port))
 		self.socket.listen(1)
 
 		while True:
+			connection, address = self.socket.accept()
+			client_ip = address[0]
+			print("\nNew connection: ", client_ip, '\n')
 
-			conn, address = self.socket.accept()
-			client = address[0] + ':' + str(address[1])
-
-			print("\nNew connection: ", client, '\n')
-
-			if src.db.blacklist.exists(client):
-				"""
-				if a specific instance on this (IP : PORT) is misbehaving
-				"""
-				conn.shutdown(socket.SHUT_RDWR)
-				conn.close()
-
-			elif src.db.authenticated_clients.exists(client):
-				"""
-				if a client on this (IP : PORT) got disconnected somehow and
-				wants to reconnect now
-				"""
-				process = multiprocessing.Process( \
-					target=src.requests.reconnect, args=(conn,))
-				process.daemon = True
-				process.start()
-
-			elif src.db.blacklist.exists(address[0]):
-				"""
-				if a client has launched multiple instances from (IP : XXXX) and
-				is misbehavings. Moreover, how many is too many?
-				"""
-				conn.shutdown(socket.SHUT_RDWR)
-				conn.close()
-
+			if blacklist.exists(client_ip):
+				# if a specific instance on this IP is misbehaving
+				connection.shutdown(SHUT_RDWR)
+				connection.close()
 			else:
-				"""
-				if it is a new client trying to connect to the sevice
-				"""
-				process = multiprocessing.Process( \
-					target=src.client.unauthenticated_client_greeter, \
-					args=(conn,))
+				# if it is a new client trying to connect to the sevice
+				process = Process(target=\
+					src.client.unauthenticated_client_handler, \
+					args=(connection,))
 				process.daemon = True
 				process.start()
 
@@ -75,9 +41,4 @@ def main():
 		server_.start()
 	finally:
 		src.shutdown.server()
-		for process in multiprocessing.active_children():
-			print("Shutting down process: ", process)
-			process.terminate()
-			process.join()
-		# shutdown background threads.
 	print("All done")
