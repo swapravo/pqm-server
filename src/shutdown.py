8db1e4900e48758388149783e0653781ff2b234b
Swapravo Sinha Roy <swapravo@tutanota.com>
2021-02-24 13:31:33 +0530

    Still needs the process killers

--- src/shutdown.py
+++ src/shutdown.py
@@ -1,9 +1,13 @@
 from time import sleep
+import multiprocessing
 
 
 def server():
     print("Shutting server down...")
-    pass
+	for process in multiprocessing.active_children():
+		print("Shutting down process: ", process)
+		process.terminate()
+		process.join()
 
 
 def process():
